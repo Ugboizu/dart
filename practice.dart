133fa39fd34c6a9ef53be51fef8cd0fcca0278ee
ugboizu <umezugbo@gmail.com>
2023-08-23 09:07:18 +0100

    my third commit

--- practice.dart
+++ practice.dart
@@ -3,6 +3,9 @@ main() {
   List<int> evenNumbers = [];
   sayEven(evenNumbers);
   print(evenNumbers);
+
+  // List<int> oddNumbers = [];
+  sayOdd();
 }
 void sayEven(List<int> evenNumberlist) {
   int num = 50;
@@ -10,6 +13,16 @@ void sayEven(List<int> evenNumberlist) {
   for(int i = 1; i <= num; i++) {
     if(i % 2 == 0) {
       evenNumberlist.add(i);
+    }
+  }
+}
+
+void sayOdd() {
+  int num = 50;
+  print("The odd numbers between 1 and 50 are: ");
+  for(int i=1; i<=num; i++) {
+    if(i % 2 == 1) {
+      print(i);
     }
   }
 }
