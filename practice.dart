07c870f1630cc5ed79afe3e5c0bff391bd159726
ugboizu <umezugbo@gmail.com>
2023-08-23 09:00:32 +0100

    my second commit

--- practice.dart
+++ practice.dart
@@ -1,13 +1,15 @@
-main(List<String> arguments) {
+main() {
   // Write a simple function to print out all even numbers between 1 and 50
-  sayEven();
+  List<int> evenNumbers = [];
+  sayEven(evenNumbers);
+  print(evenNumbers);
 }
-void sayEven() {
+void sayEven(List<int> evenNumberlist) {
   int num = 50;
   print("The even numbers between 1 and 50 are: ");
   for(int i = 1; i <= num; i++) {
     if(i % 2 == 0) {
-      print(i);
+      evenNumberlist.add(i);
     }
   }
 }
