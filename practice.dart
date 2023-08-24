e2c79b3399e004f63cdc716aca74475e013049ef
UncleSamTech <samueloel3486@gmail.com>
2023-08-24 08:29:14 -0600

    wip

--- practice.dart
+++ practice.dart
@@ -1,32 +1,36 @@
-main() {
-  // Write a simple function to print out all even numbers between 1 and 50
+
+List<int> sayEven() {
   List<int> evenNumbers = [];
-  sayEven(evenNumbers);
-  print(evenNumbers);
-
-  List<int> oddNumbers = [];
-  sayOdd(oddNumbers);
-  print(oddNumbers);
-
-  evenNumbers.addAll(oddNumbers);
-  print(evenNumbers);
-}
-void sayEven(List<int> evenNumberlist) {
   int num = 50;
-  print("The even numbers between 1 and 50 are: ");
   for(int i = 1; i <= num; i++) {
     if(i % 2 == 0) {
-      evenNumberlist.add(i);
+      evenNumbers.add(i);
     }
   }
+  print("The even numbers between 1 and 50 are: ${evenNumbers}");
+  return evenNumbers;
 }
 
-void sayOdd(List<int> oddNumberlist) {
+List<int> sayOdd() {
+  List<int> oddNumbers = [];
   int num = 50;
-  print("The odd numbers between 1 and 50 are: ");
+  
   for(int i=1; i<=num; i++) {
-    if(i % 2 == 1) {
-      oddNumberlist.add(i);
+    if(i % 2 != 0) {
+      oddNumbers.add(i);
     }
   }
+  print( "The odd numbers between 1 and 50 are: ${oddNumbers}");
+  return oddNumbers;
+
+}
+
+main() {
+  // Write a simple function to print out all even numbers between 1 and 50
+  
+  sayEven();
+
+  sayOdd();
+  
+  
 }
